f0450c8f6edb8fa9b4c931be18a8048f08219ef8
Better Mistakes <123073428+bettermistakes@users.noreply.github.com>
2025-03-12 16:16:48 +0000

    Update Home.js

--- Home.js
+++ Home.js
@@ -120,7 +120,7 @@ function slider1() {
           },
           scrollTrigger: {
             trigger: fadeFlashElem, // Trigger based on container
-            start: "top bottom+=300", // Slightly before visibility
+            start: "top bottom+=200", // Slightly before visibility
             markers: false, // Debug markers off
           },
         }
